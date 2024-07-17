d78a5fc12434019bba9471f8c424504be54988a9
Lucas Hebmuller <108936216+LucasHebmuller@users.noreply.github.com>
2024-07-16 22:43:35 -0300

    update GPA

---
+++ script.js
@@ -0,0 +1,7 @@
+let cumulative_gpa = document.querySelector('div#cumulative_gpa h2')
+let overall_gpa = 0
+cumulative_gpa.innerHTML += `<span class="number">${overall_gpa}</span>`
+
+let term_gpa = document.querySelector('div.term_gpa h3')
+let semester_gpa = 0
+term_gpa.innerHTML += `<span class="number">${semester_gpa}</span>`
