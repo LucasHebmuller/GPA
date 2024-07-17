3ad00cdac14b40b760dfceb2dadba30123a9caa9
Lucas Hebmuller <108936216+LucasHebmuller@users.noreply.github.com>
2024-07-17 19:49:08 -0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,62 @@
-let cumulative_gpa = document.querySelector('div#cumulative_gpa h2')
-let overall_gpa = 0
-cumulative_gpa.innerHTML += `<span class="number">${overall_gpa}</span>`
+let overallGpa = 0
+let termCount = 0
+let termGpa = [0]
+let termPoints = [0]
+let termCredits = [0]
 
-let term_gpa = document.querySelector('div.term_gpa h3')
-let semester_gpa = 0
-term_gpa.innerHTML += `<span class="number">${semester_gpa}</span>`
+function enterGrade() {
+    let grade = document.getElementById('igrade')
+    let credits = document.getElementById('icredits')
+
+    termCredits[termCount] += credits
+
+    if(grade == "A")
+        termPoints[termCount] += (4 * credits)
+    else if(grade == "A-")
+        termPoints[termCount] += (3.7 * credits)
+    else if(grade == "B+")
+        termPoints[termCount] += (3.3 * credits)
+    else if(grade == "B")
+        termPoints[termCount] += (3 * credits)
+    else if(grade == "B-")
+        termPoints[termCount] += (2.7 * credits)
+    else if(grade == "C+")
+        termPoints[termCount] += (2.3 * credits)
+    else if(grade == "C")
+        termPoints[termCount] += (2 * credits)
+    else if(grade == "C-")
+        termPoints[termCount] += (1.7 * credits)
+    else if(grade == "D+")
+        termPoints[termCount] += (1.3 * credits)
+    else if(grade == "D")
+        termPoints[termCount] += (1 * credits)
+    else if(grade == "D-")
+        termPoints[termCount] += (0.7 * credits)
+    else if(grade == "F")
+        termPoints[termCount] += (0 * credits)
+
+
+}
+
+function calculateTermGpa() {
+    return termPoints[termCount] / termCredits[termCount]
+}
+
+function calculateOverallGpa() {
+    let allPoints = 0
+    let allCredits = 0
+
+    for (let i = 0; i <= termCount; i++) {
+        allPoints += termPoints[i]
+        allCredits += termCredits[i]
+    }
+
+    return allPoints / allCredits
+}
+
+
+let overallContainer = document.querySelector('div#cumulative_gpa h2')
+overallContainer.innerHTML += `<span class="number">${overallGpa}</span>`
+
+let termContainer = document.querySelector('div.term_gpa h3')
+termContainer.innerHTML += `<span class="number">${termGpa[termCount]}</span>`
