48b7fc4390117e7a84a95db5190e20d4fa8be3fe
Lucas Hebmuller <108936216+LucasHebmuller@users.noreply.github.com>
2024-08-05 15:56:51 -0300

    update

--- script.js
+++ script.js
@@ -3,7 +3,74 @@ let courseCount = [4] // course for each semester
 
 function calculateSemesterGPA(form) {
     const grades = form.querySelectorAll('.igrade')
+    let points = 0
+    let credits = 0
+
+    grades.forEach(grade => {
+        const value = grade.value
+        if (value) {
+            switch(value) {
+                case 'A':
+                    points += 4.0
+                    break
+                case 'A-':
+                    points += 3.7
+                    break
+                case 'B+':
+                    points += 3.3
+                    break
+                case 'B':
+                    points += 3.0
+                    break
+                case 'B-':
+                    points += 2.7
+                    break
+                case 'C+':
+                    points += 2.3
+                    break
+                case 'C':
+                    points += 2.0
+                    break
+                case 'C-':
+                    points += 1.7
+                    break
+                case 'D+':
+                    points += 1.3
+                    break
+                case 'D':
+                    points += 1.0
+                    break
+                case 'F':
+                    points += 0.0
+                    break
+                default:
+                    points += 0.0
+                    break
+            }
+        }
+
+        const credit = form.querySelectorAll('.icredits')
+        credits += credit
+    })
+
+    const semesterGPA = points / credits
+
+    let semesterContainer = form.querySelector('div.semester_gpa h3')
+    semesterContainer.innerHTML += `<span class="number">${semesterGPA}</span>`
+    semesterContainer.innerHTML += 'teste'
+
+    calculateOverallGPA()
 }
+
+function calculateOverallGPA() {
+
+}
+
+// Initial setup: add event listeners to existing course inputs
+document.querySelectorAll('.igrade').forEach(input => {
+    input.addEventListener('input', () => calculateSemesterGPA(input.closest('.semester_gpa')))
+})
+    
 
 /*
 let overallGpa = 0
@@ -42,22 +109,6 @@ function enterGrade() {
         termPoints[termCount] += (0.7 * credits)
     else if(grade == "F")
         termPoints[termCount] += (0 * credits)
-}
-
-function calculateTermGpa() {
-   return termPoints[termCount] / termCredits[termCount]
-}
-
-function calculateOverallGpa() {
-    let allPoints = 0
-    let allCredits = 0
-
-    for (let i = 0; i <= termCount; i++) {
-        allPoints += termPoints[i]
-        allCredits += termCredits[i]
-    }
-
-    return allPoints / allCredits
 }
 
 let overallContainer = document.querySelector('div#cumulative_gpa h2')
